95005a31fbe85d6b51c8e8ca1df3c3a25ba5194b
Nick Stenning <nick.stenning@digital.cabinet-office.gov.uk>
2012-08-16 17:52:03 +0100

    Add definition (untested) for precise

--- definitions/govuk_dev_precise64/postinstall.sh
+++ definitions/govuk_dev_precise64/postinstall.sh
@@ -7,13 +7,16 @@ date > /etc/vagrant_box_build_time
 apt-get -y update
 apt-get -y upgrade
 apt-get -y install linux-headers-$(uname -r) build-essential
-apt-get -y install zlib1g-dev libssl-dev libreadline5-dev
+apt-get -y install zlib1g-dev libssl-dev libreadline-gplv2-dev
+apt-get -y install curl
 apt-get clean
 
 # Setup sudo to allow no-password sudo for "admin"
 cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
+groupadd -r admin || true
+usermod -a -G admin vagrant
 
 # Install NFS client
 apt-get -y install nfs-common
